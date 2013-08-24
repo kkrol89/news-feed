13d0ae1797b20f5508183220baf4f2e943c338d6
Konrad Król <konrad.krol89@gmail.com>
2013-08-24 17:55:41 +0200

    Added twitter-bootstrap; added top panel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def control_state(entity, field)
+    entity.errors[field].present? ? 'error' : 'valid'
+	end
+
+	def field_errors(entity, field)
+    errors = entity.errors[field]    
+    errors.present? ? errors.join(";") : nil
+  end
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,4 @@
 class News < ActiveRecord::Base
   attr_accessible :author, :content, :title
+  validates :title, :author, :content, presence: true
 end
